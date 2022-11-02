fe129413739d2f1001b2372bbcf748f4f14bad7d
Calendau GUQUET <cguquet@synexie.fr>
2022-11-03 00:53:42 +0100

    Neuron & InputSynapse (tests are missing)

---
+++ src/ArtificialNeuralNetwork.Library/InputSynapse.cs
@@ -0,0 +1,33 @@
+﻿using ArtificialNeuralNetwork.Abstractions;
+
+namespace ArtificialNeuralNetwork.Library;
+
+public class InputSynapse : ISynapse
+{
+    private readonly INeuron _to;
+
+    public double Weight { get; set; }
+    public double PreviousWeight { get; set; }
+    public double Output { get; set; }
+
+    public InputSynapse(INeuron to)
+    {
+        _to = to;
+        Weight = 1;
+        PreviousWeight = 1;
+    }
+
+    public InputSynapse(INeuron to, double inputValue) : this(to)
+    {
+        Output = inputValue;
+    }
+
+    public double GetOutput() => Output;
+
+    public bool IsFromNeuron(Guid fromNeuronId) => false;
+
+    public void UpdateWeight(double learningRate, double delta)
+    {
+        throw new InvalidOperationException("Input synapses have immutable weight value of 1.0");
+    }
+}
---
+++ src/ArtificialNeuralNetwork.Library/Neuron.cs
@@ -0,0 +1,62 @@
+﻿using ArtificialNeuralNetwork.Abstractions;
+
+namespace ArtificialNeuralNetwork.Library;
+
+public class Neuron : INeuron
+{
+    private readonly IActivationFunction _activationFunction;
+    private readonly IInputFunction _inputFunction;
+
+    public Guid Id { get; }
+    public double PreviousPartialDerivate { get; set; }
+    public List<ISynapse> Inputs { get; set; } = new();
+    public List<ISynapse> Outputs { get; set; } = new();
+
+    public Neuron(IActivationFunction activationFunction, 
+                  IInputFunction inputFunction)
+    {
+        Id = Guid.NewGuid();
+
+        _activationFunction = activationFunction;
+        _inputFunction = inputFunction;
+    }
+
+    public void AddInputNeuron(INeuron inputNeuron)
+    {
+        ISynapse inputSynapse = new Synapse(inputNeuron, this);
+
+        Inputs.Add(inputSynapse);
+
+        inputNeuron.Outputs.Add(inputSynapse);
+    }
+
+    public void AddOutputNeuron(INeuron outputNeuron)
+    {
+        ISynapse outputSynapse = new Synapse(this, outputNeuron);
+
+        Outputs.Add(outputSynapse);
+
+        outputNeuron.Inputs.Add(outputSynapse);
+    }
+
+    public double CalculateOutput()
+    {
+        var inputValue = _inputFunction.CalculateInput(Inputs);
+
+        return _activationFunction.CalculateOutput(inputValue);
+    }
+
+    public void AddInputSynapse(double inputValue)
+    {
+        ISynapse inputSynapse = new InputSynapse(this, inputValue);
+
+        Inputs.Add(inputSynapse);
+    }
+
+    public void PushValueOnInput(double inputValue)
+    {
+        InputSynapse inputSynapse = (InputSynapse)Inputs.First(x => x.GetType() == typeof(InputSynapse));
+
+        inputSynapse.Output = inputValue;
+    }
+}
